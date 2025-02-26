ab399777b1c856eebc59966a2966ef9447858407
dno-luigi <137290593+dno-luigi@users.noreply.github.com>
2025-02-26 09:45:49 -0800

    Update cf.ts

--- src/cf.ts
+++ src/cf.ts
@@ -1,33 +1,46 @@
-import { Ai } from "@cloudflare/ai";
-import { Context } from "hono";
-import { Buffer } from "buffer";
-import { stream } from "hono/streaming";
+// src/cf.ts
+import * as buffer from 'buffer';
 
-const ssePrefix = "data:";
+const ssePrefix = 'data:';
 const sseEnd = `${ssePrefix} [DONE]`;
 
-export default async function workerAI(
-  c: Context,
-  model: any,
-  messages: any[],
-) {
-  console.log(c.env.AI);
-  const ai = new Ai(c.env.AI);
-  const response = await ai.run(
-    model, // models[modelIndex],
-    {
-      messages,
-      stream: true,
-    },
-  );
-  return stream(c, async (stream) => {
-    for await (const chunk of response as any) {
-      const str = Buffer.from(chunk).toString("utf-8");
-      if (str != sseEnd) {
-        const jsonstring = str.substring(ssePrefix.length);
-        const resObj = JSON.parse(jsonstring);
-        await stream.write(resObj.response);
-      }
-    }
-  });
+import { type Context } from 'hono';
+import { type Ai } from '@cloudflare/ai';
+
+export class W {
+  binding: any;
+  options: {};
+  logs: any;
+  lastRequestId: string;
+  constructor(e: any, t = {}) {
+    if (!e)
+      throw new Error('Ai binding is undefined. Please provide a valid binding.');
+    this.binding = e;
+    this.options = t;
+    this.lastRequestId = '';
+  }
+  async run(e: any, t: any) {
+    console.log(e, t);
+    const s = await this.binding.run(e, t, this.options);
+    return (
+      (this.lastRequestId = this.binding.lastRequestId),
+      this.options.debug && (this.logs = this.binding.getLogs()),
+      s
+    );
+  }
+  getLogs() {
+    return this.logs;
+  }
 }
+/**
+ *  Fixes for TypeScript
+ */
+interface Context extends Context {
+  env: {
+    AI: Ai;
+    PKV: KVNamespace;
+  };
+  get: <T>(key: string) => T;
+  set: <T>(key: string, value: T) => void;
+}
+
